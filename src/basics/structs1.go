c1f3012c0dda5518ec77d662d68e2fb260cfad1a
Ferran Orriols <ferran.orriols@gmail.com>
2016-10-08 10:08:13 +0200

    Added anonymous struct

--- src/basics/structs1.go
+++ src/basics/structs1.go
@@ -17,4 +17,19 @@ func main() {
 	}
 	fmt.Printf("%T %v\n", p, p) //main.person {Ferran 30}
 	fmt.Printf("%T %v\n", p.age, p.age) //int 30
+
+	//Defining an anonymous struct which contains a previous defined struct
+	p2 := struct {
+		username string
+		person
+	} {
+		username: "pedro82",
+		person: person {
+			name: "Pedro",
+			age: 34,
+		},
+	}
+	fmt.Printf("%T %v\n", p2, p2)
+	fmt.Printf("%T %v\n", &p2, &p2) //*struct { username string; main.person } &{johnsmith {a 34}}
 }
+
